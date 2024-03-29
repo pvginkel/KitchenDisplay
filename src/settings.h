15f39b013fce1edb98d2bd06b53c35fa30ba6584
Pieter van Ginkel <pvginkel@gmail.com>
2024-03-29 20:48:19 +0100

    Decreased padding.

--- src/settings.h
+++ src/settings.h
@@ -5,4 +5,4 @@ constexpr char const* SRVMAIN = "192.168.178.3";
 constexpr char const* FAVORITE_LABEL = "FAVORIET";
 constexpr char const* ICU_LOCALE = "nl-NL";
 constexpr auto HOME_UI_COLUMNS = 3;
-constexpr auto PADDING = 26;
+constexpr auto PADDING = 20;
